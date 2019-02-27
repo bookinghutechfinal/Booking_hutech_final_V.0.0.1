568c5bf6893014ab9a1417c64dd83f22809436b8
blue20297 <blue20297@gmail.com>
2019-02-27 16:48:44 +0700

    logout

---
+++ BookingHutech/wwwroot/controller/account/logoutController.js
@@ -0,0 +1,11 @@
+﻿ 
+mainmodule.controller('LogoutController', ['$scope', '$state', '$rootScope', '$modal', '$http', '$cookies', 'toastr', '$dao',  
+    function ($scope, $state, $rootScope, $modal, $http, $cookies, toastr, $dao) {
+        $scope.UserName = "Trần Nhựt Anh";
+        
+        $scope.logout = function () {
+            
+        }
+
+
+    }]);  
